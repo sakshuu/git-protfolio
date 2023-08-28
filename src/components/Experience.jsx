fa5d683008485d90877883a48c238730d3bf0951
sakshi jadhav <sakshisjadhav.120@gmail.com>
2023-08-28 18:46:58 +0530

    updated

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,19 +5,13 @@ import "./../assets/css/resume.css"
 const Experience = () => {
 
   const experiences = [
-    {
-      year:"aug 2023 - present",  
-      compunyName:"Numetry Technologies",  
-      role:"Worked as frontend Developer (Traineeship)",  
-      desc:"",
-      id:"1"
-    },
+
     {
       year:"January 2023 - july 2023",  
       compunyName:"Skillhub It Solutions",  
       role:"Worked as React Js & UI intern",  
       desc:"During my internship, I worked on an e-commerce project, managing state with Redux, implementing authentication, integrating APIs with Axios, and utilizing UI libraries like Material UI and Bootstrap for improved design and user experience. Skills - ReactJS, Redux, NodeJS, MongoDB, Material-UI, JS, CSS.",
-      id:"2"
+      id:"1"
     }
    ];
   return <>
